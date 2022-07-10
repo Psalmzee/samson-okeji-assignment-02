9d6fef4a20ec37503754ff041f8620f117f3b61c
Samson Okeji <67550977+Psalmzee@users.noreply.github.com>
2022-07-10 22:41:37 +0000

    Attempted challenge-1 and challenge-2

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,13 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let sum = 0;
+  for (const number of arrayOfNumbers){
+     sum += number;
+  }
+    return sum;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+ assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +32,21 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  
+  let evenNumberCount = 0;
+
+  for (var count =0; count < arrayOfNumbers.length; count++){
+    if (arrayOfNumbers[count] % 2 === 0){
+      evenNumberCount++;
+    } else {
+      
+      continue;
+      
+    }
+  }
+    return evenNumberCount;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+ assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
